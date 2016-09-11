38ee9e4a401ee8c2ba10f61c9e6da6666a2ad139
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2016-09-11 14:04:36 +0200

    Update config to use base jekyll image

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM jekyll/jekyll:pages
-
-RUN apk update && apk add tzdata && cp /usr/share/zoneinfo/Europe/Oslo /etc/localtime && echo "Europe/Oslo" > /etc/timezone && apk del tzdata
--- dev_server.sh
+++ dev_server.sh
@@ -2,4 +2,4 @@
 
 # This script simply spins up a jekyll instance to serve the blog during development/writing/whatever it's called when blogging.
 
-docker run --rm --name=jonasws.github.io --volume=$(pwd):/srv/jekyll -it -p 4000:4000 jonasws/jekyll:pages
+docker run --rm --name=jonasws.github.io --volume=$(pwd):/srv/jekyll -it -p 4000:4000 jekyll/jekyll:pages
