f5f926769abb235a16fd3c01f93e2333691001a2
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2017-06-14 15:37:01 +0200

    Correct wording of dev_server.sh comments

--- dev_server.sh
+++ dev_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-# This script simply spins up a jekyll instance to serve the blog during development/writing/whatever it's called when blogging.
+# This script simply spins up a jekyll container to serve the blog during development/writing/whatever it's called when blogging.
 
 docker run --rm --name=jonasws.github.io --volume=$(pwd):/usr/src/app -it -p 4000:4000 starefossen/github-pages
